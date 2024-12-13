75dc721e043a5c3383c9382c1207ba9b5dec8e53
Josue <josue>
2024-12-13 11:45:17 +0100

    Dashboard completed step 1

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,6 +1,29 @@
 <template>
     <div>
-        <h1>Dashboard</h1>
+        <table class="table-responsive">
+            <thead>
+                <tr>
+                    <th>Cours</th>
+                    <th>Horaire</th>
+                    <th>Enseigant</th>
+                    <th>Participant</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Anglais</td>
+                    <td>10:00 - 12:00</td>
+                    <td>Samuel</td>
+                    <td>Etudiant</td>
+                </tr>
+            </tbody>
+            <tfoot></tfoot>
+            
+            <tr>
+
+            </tr>
+            
+        </table>
     </div>
 </template>
 
